14e1a63d3acd0931bf54f23eb4e39e9df7536646
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-09-04 09:42:31 +0000

    完了

--- rails6-bookers2-debug-ruby3-main/app/controllers/users_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/users_controller.rb
@@ -13,13 +13,18 @@ class UsersController < ApplicationController
   end
 
   def edit
-     @user = User.find(params[:id])
+       @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
+     @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
+      @book = Book.new
       render :edit
     end
   end
